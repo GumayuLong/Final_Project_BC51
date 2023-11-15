e7b81022ea4e124288d9f941b62925fa231aa3fa
GumayuLong <tothanhlong2002@gmail.com>
2023-11-15 10:58:00 +0700

    dat phong chua check lich dat

--- src/services/bookRoomService.js
+++ src/services/bookRoomService.js
@@ -1,19 +1,29 @@
+/** @format */
+
 import { request } from "../configs/api";
 
 class BookRoomService {
-    fetchBookedRoomFromUserApi(userId) {
-        return request({
+	fetchBookedRoomFromUserApi(userId) {
+		return request({
 			url: `/dat-phong/lay-theo-nguoi-dung/${userId}/`,
 			method: "GET",
 		});
-    }
+	}
 
-    fetchListBookedRoomApi() {
-        return request({
+	fetchListBookedRoomApi() {
+		return request({
 			url: "/dat-phong",
 			method: "GET",
 		});
-    }
+	}
+
+	bookRoomApi(data) {
+		return request({
+			url: "/dat-phong",
+			method: "POST",
+      data
+		});
+	}
 }
 
 export const bookRoomService = new BookRoomService();
